99dcbaae91e2925d0e0761bf434fd31cb9e6bd24
James Brown <james.brown1417@gmail.com>
2022-07-19 16:57:52 +0930

    finished web scraping FA data

--- Scripts/2022-Season/Fantasy-Coach-Performance/Web-Scraping-Scripts/web-scrape-fa-transactions.R
+++ Scripts/2022-Season/Fantasy-Coach-Performance/Web-Scraping-Scripts/web-scrape-fa-transactions.R
@@ -44,57 +44,145 @@ get_transactions <- function(html_path) {
   return(transactions)
 }
 
-#===============================================================================
-# Create function that reads html and outputs teams table
-#===============================================================================
+##%######################################################%##
+#                                                          #
+####         Get list of scraped text (in form          ####
+####       of a character vector) for each round        ####
+#                                                          #
+##%######################################################%##
 
-get_teams <- function(html_path) {
-  # Read HTML from path
-  full_path <- paste("Data/HTML-files/teams/", html_path, sep = "")
-  html_source <- read_html(full_path)
+transactions_by_round <- map(transaction_files, get_transactions)
+
+##%######################################################%##
+#                                                          #
+####      Function to extract transaction details       ####
+#                                                          #
+##%######################################################%##
+
+extract_transaction <- function(transaction_vector) {
   
-  # Create teams vector and return
-  teams <-
-    html_source %>%
-    html_elements(".jfIbnL") %>%
-    html_text2()
-  
-  return(teams)
+# Process Strings
+transaction_vector <- str_remove_all(transaction_vector, "(\n)|(Uncertain)|(the Free Agent:)|(Close Circle Logo)|(Transactions)|(\\.)|(Injured)|(the Restricted Free Agent)")
+transaction_vector <- str_replace_all(transaction_vector, "(Defender)|(Forward)|(Midfielder)|(Ruck)|(:)", " ")
+transaction_vector <- str_replace_all(transaction_vector, "(Def)|(Fwd)|(Mid)|(Ruck)|(:)", " ")
+transaction_vector <- str_replace_all(transaction_vector, "/[ ]{1,}[ ]", "")
+transaction_vector <- str_replace_all(transaction_vector, "[ ]{2,}", " ")
+transaction_vector <- transaction_vector[str_detect(transaction_vector, "initiated and traded", negate = TRUE)]
+
+# Extract Dates
+dates <- str_extract_all(transaction_vector, "^.* 2022") %>% str_replace_all(" ", "-")
+
+# Extract Team
+team <- str_extract_all(transaction_vector, "(?<=2022)[A-Za-z4 ']{5,31}(?= selected| delisted)")
+
+# Get Selected Players
+selected <- str_extract_all(transaction_vector, "(?<=selected )[A-Za-z '-]{5,30}(?= And | $)")
+selected[sapply(selected, is_empty)] <- NA
+
+# Get Delisted Players
+delisted <- str_extract_all(transaction_vector, "(?<=delisted )[A-Za-z '-]{5,30}(?= $)")
+delisted[sapply(delisted, is_empty)] <- NA
+
+# Create Output Tibble
+selected <- tibble(date = dates, team = unlist(team), player_name = unlist(selected), type = "selected")
+delisted <- tibble(date = dates, team = unlist(team), player_name = unlist(delisted), type = "delisted")
+transaction_df <- bind_rows(selected, delisted) %>% filter(!is.na(player_name))
+transaction_df$date <- as.Date(transaction_df$date, "%d-%B-%Y")
+
+return(transaction_df)
+
 }
 
 ##%######################################################%##
 #                                                          #
-####                        Test                        ####
+####   Apply function to extract transasction details   ####
+####      to list of transaction character vectors      ####
 #                                                          #
 ##%######################################################%##
 
-# Strings
-a <- get_transactions("round_01_transactions.html")
-a <- str_remove_all(a, "(\n)|(Uncertain)|(the Free Agent:)|(Close Circle Logo)|(Transactions)|(\\.)|(Injured)|(the Restricted Free Agent)")
-a <- str_replace_all(a, "(Defender)|(Forward)|(Midfielder)|(Ruck)|(:)", " ")
-a <- str_replace_all(a, "(Def)|(Fwd)|(Mid)|(Ruck)|(:)", " ")
-a <- str_replace_all(a, "/[ ]{1,}[ ]", "")
-a <- str_replace_all(a, "[ ]{2,}", " ")
-a
+all_transactions <- map(transactions_by_round, extract_transaction)
+all_transactions <- bind_rows(all_transactions)
 
-# Dates
-dates <- str_extract_all(a, "^.* 2022") %>% str_replace_all(" ", "-")
-dates
+##%######################################################%##
+#                                                          #
+####          Add Stats to transactions table           ####
+#                                                          #
+##%######################################################%##
 
+# Get averages
+fantasy_averages <-
+combined_stats_table %>%
+  filter(round_number < "Round 18") %>%
+  filter(TOG > 50) %>%
+  summarise(`Avg Points` = mean(fantasy_points), games_played = max(games_played)) %>%
+  mutate(player_name = str_replace(player_name, "\\.", "")) %>%
+  mutate(player_name = str_replace(player_name, "Paddy McCartin", "Patrick McCartin")) %>%
+  mutate(player_name = str_replace(player_name, "Josh Rachele", "Joshua Rachele"))
 
-# Team
-team <- str_extract_all(a, "(?<=2022)[A-Za-z ']{5,31}(?= selected| delisted)")
+# Join with main table
+all_transactions <-
+  all_transactions %>%
+  left_join(fantasy_averages)
 
-# Player Selected
-selected <- str_extract_all(a, "(?<=selected )[A-Za-z '-]{5,30}(?= And | $)")
-selected[sapply(selected, is_empty)] <- NA
+##%######################################################%##
+#                                                          #
+####                Analyse Transactions                ####
+#                                                          #
+##%######################################################%##
 
-# Player Delisted
-delisted <- str_extract_all(a, "(?<=delisted )[A-Za-z '-]{5,30}(?= $)")
-delisted[sapply(delisted, is_empty)] <- NA
+# Total Transactions by Team
+all_transactions %>%
+  filter(type == "selected") %>%
+  group_by(team) %>%
+  tally() %>%
+  arrange(desc(n))
 
-# Round 1 tibble
-selected <- tibble(date = dates, team = unlist(team), player_name = unlist(selected), type = "selected")
-delisted <- tibble(date = dates, team = unlist(team), player_name = unlist(delisted), type = "delisted")
-round_1_transactions <- bind_rows(selected, delisted) %>% filter(!is.na(player_name))
-round_1_transactions$date <- as.Date(round_1_transactions$date, "%d-%B-%Y")
+# Total Transactions by player (selected)
+all_transactions %>%
+  filter(type == "selected") %>%
+  group_by(player_name) %>%
+  tally() %>%
+  arrange(desc(n))
+
+# Total Transactions by player (delisted)
+all_transactions %>%
+  filter(type == "delisted") %>%
+  group_by(player_name) %>%
+  tally() %>%
+  arrange(desc(n))
+
+# Each team's most selected player
+all_transactions %>%
+  filter(type == "selected") %>%
+  group_by(team, player_name) %>%
+  tally() %>%
+  group_by(team) %>%
+  arrange(desc(n)) %>%
+  slice(1)
+
+# Each team's most delisted player
+all_transactions %>%
+  filter(type == "delisted") %>%
+  group_by(team, player_name) %>%
+  tally() %>%
+  group_by(team) %>%
+  arrange(desc(n)) %>%
+  slice(1)
+
+# Costly Delistings
+all_transactions %>%
+  arrange(date) %>%
+  group_by(team, player_name) %>%
+  filter(row_number() == n()) %>%
+  ungroup() %>%
+  filter(type == "delisted") %>%
+  arrange(desc(`Avg Points`))
+
+# Best Pickups
+all_transactions %>%
+  arrange(date) %>%
+  group_by(team, player_name) %>%
+  filter(row_number() == n()) %>%
+  ungroup() %>%
+  filter(type == "selected") %>%
+  arrange(desc(`Avg Points`))
